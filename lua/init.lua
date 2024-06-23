39c804c93f87e55977920abc40b78000dc1a8a90
Sam Erickson <samericksn@gmail.com>
2024-06-22 21:37:04 -0700

    feat: add fetching titles of links on paste

---
+++ lua/init.lua
@@ -0,0 +1,36 @@
+local M = {}
+
+function M.formatPaste()
+  local pasteRegister = vim.fn.getreg("*")
+
+  local isUrl = string.match(pasteRegister, "^[a-z]*://[^ >,;]*$")
+
+  if not isUrl then
+    return pasteRegister
+  end
+
+  local curl = require("plenary.curl")
+  local res = curl.request({
+    url = pasteRegister,
+    method = "get",
+    accept = "application/json",
+  })
+
+  local title = string.match(res.body, "<title>(.-)</title>")
+  return string.format("[%s](%s)", title, pasteRegister)
+end
+
+function M.paste()
+  local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+  vim.api.nvim_buf_set_text(0, row - 1, col, row - 1, col, { M.formatPaste() })
+end
+
+function M.setup()
+  vim.api.nvim_create_user_command("MarkdownLinkPaste", function()
+    pcall(function()
+      M.paste()
+    end)
+  end, {})
+end
+
+return M
