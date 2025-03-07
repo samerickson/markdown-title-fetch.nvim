932c0eb9973a5c6ac885527be64fa92c85b3af30
Sam Erickson <erickson.samuel@outlook.com>
2025-03-06 16:57:31 -0800

    feat: support async title collection and buffer modification

--- lua/init.lua
+++ lua/init.lua
@@ -1,46 +1,94 @@
 local M = {}
 
----Gets contents of '*' register and formats it as a markdown link if the contents are a link
----fetching the title of the webpage automatically. If the webpage title cannot be found,
----then the domain name will be used.
----@private
----@return string formatted url if contents of * are a url, and contents of * as is otherwise
-function M.formatPaste()
-  local pasteRegister = vim.fn.getreg("*")
+--- Fetch tile for webpage using curl
+---@param url string - url fetch title of
+---@param callback function - to call with result of fetching title
+local function fetch_title(url, callback)
+  vim.system({ "curl", "-s", url }, { text = true }, function(result)
+    if result.code ~= 0 then
+      vim.notify("Failed to fetch URL: " .. url, vim.log.levels.ERROR)
+      return
+    end
 
-  local isUrl = string.match(pasteRegister, "^[a-z]*://[^ >,;]*$")
-
-  if not isUrl then
-    return pasteRegister
-  end
-
-  local curl = require("plenary.curl")
-  local res = curl.request({
-    url = pasteRegister,
-    method = "get",
-  })
-
-  local title = res.body:match("<title>(.-)</title>")
-
-  if title == nil then
-    title = pasteRegister:match("^%w+://([^/]+)")
-  end
-
-  return string.format("[%s](%s)", title, pasteRegister)
+    -- Extract title from HTML
+    local title = result.stdout:match("<title>(.-)</title>")
+    if title then
+      title = title:gsub("\n", " "):gsub("^%s*(.-)%s*$", "%1") -- Trim whitespace
+      callback(title)
+    else
+      vim.notify("No title found for URL: " .. url, vim.log.levels.WARN)
+    end
+  end)
 end
 
----Pastes the contents of the '*' register after formatting
----@private
-function M.paste()
-  local row, col = unpack(vim.api.nvim_win_get_cursor(0))
-  vim.api.nvim_buf_set_text(0, row - 1, col, row - 1, col, { M.formatPaste() })
+--- Formats a URL in a way that can be string replaced with gsub()
+---@param str string string to escape patterns on
+---@return string
+---@return integer count
+local function escape_pattern(str)
+  return str:gsub("([%.%-%+%*%?%[%]%^%$%(%)%{%,%}])", "%%%1")
+end
+
+--- Formats links in clipboard register "+" as a markdown link if contents
+--- are a link to an html page that contains a <title></title>
+local function on_paste()
+  print("here")
+
+  -- If we are not in a markdown file then do nothing
+  if vim.bo.filetype ~= "markdown" then
+    return
+  end
+
+  vim.schedule(function()
+    -- Get cursor position
+    local bufnr = vim.api.nvim_get_current_buf()
+    local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+
+    -- TODO: make this overridable via config. You might want to use *
+    local line = vim.fn.getreg("+")
+
+    -- Extract URL (basic regex)
+    local url = line:match("(http[s]?://[%w%._~:/?#@!$&'()*+,;=-]+)")
+
+    if url then
+      print("is url")
+      local escaped_url = escape_pattern(url)
+      local placeholder = "%%__PLACEHOLDER__%%"
+      local new_placeholder_line = line:gsub(escaped_url, "[" .. placeholder .. "](" .. line .. ")")
+      -- __AUTO_GENERATED_PRINT_VAR_START__
+      print([==[on_paste#(anon)#if new_placeholder_line:]==], vim.inspect(new_placeholder_line)) -- __AUTO_GENERATED_PRINT_VAR_END__
+      vim.api.nvim_buf_set_lines(bufnr, row - 1, row, false, { new_placeholder_line })
+      vim.api.nvim_win_set_cursor(0, { row, col + #new_placeholder_line })
+      print("done")
+
+      fetch_title(url, function(title)
+        -- Replace the URL with a Markdown-formatted link
+        vim.schedule(function()
+          local current_line = vim.api.nvim_buf_get_lines(bufnr, row - 1, row, false)[1]
+          local new_line = current_line:gsub(placeholder, title)
+          -- __AUTO_GENERATED_PRINT_VAR_START__
+          print([==[on_paste#(anon)#if#(anon)#(anon) new_line:]==], vim.inspect(new_line)) -- __AUTO_GENERATED_PRINT_VAR_END__
+
+          vim.api.nvim_buf_set_lines(bufnr, row - 1, row, false, { new_line })
+
+          -- If the request took a while and the user is on a new line, then we want to
+          -- update the old line, then return the users cursor to the old line
+          local new_row, new_col = unpack(vim.api.nvim_win_get_cursor(0))
+          vim.api.nvim_win_set_cursor(0, { new_row, new_col + #new_line })
+        end)
+      end)
+    else
+      vim.api.nvim_buf_set_lines(bufnr, row - 1, row, false, { line })
+      vim.api.nvim_win_set_cursor(0, { row, col + #line })
+    end
+  end)
 end
 
 ---Sets up the plugin by loading 'MarkdownLinkPaste' as a command.
 function M.setup()
   vim.api.nvim_create_user_command("MarkdownLinkPaste", function()
     pcall(function()
-      M.paste()
+      on_paste()
     end)
   end, {})
 end
